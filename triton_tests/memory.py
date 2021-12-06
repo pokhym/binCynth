209ab7181488113f49379e4460fdbbb01ae85cde
pokhym <ng121@purdue.edu>
2021-12-06 02:08:40 -0500

    Update memory.py

--- triton_tests/memory.py
+++ triton_tests/memory.py
@@ -1,4 +1,4 @@
-from triton import TritonContext, Instruction, ARCH, MemoryAccess
+from triton import TritonContext, Instruction, ARCH, MemoryAccess, AST_REPRESENTATION
 
 
 code1 = [
@@ -8,7 +8,24 @@ code1 = [
 
 ctx = TritonContext()
 ctx.setArchitecture(ARCH.X86_64)
+ctx.enableSymbolicEngine(True)
 ctx.setConcreteRegisterValue(ctx.getRegister('rbp'), 0x7ffffff)
+# getSymbolicRegisters()
+rr = ctx.symbolizeRegister(ctx.getRegister('esp'), 'espsym')
+r = ctx.symbolizeRegister(ctx.getRegister('edi'), 'edisym')
+print(ctx.isRegisterSymbolized(ctx.getRegister('edi')))
+# for i in range(100):
+#     ctx.setConcreteMemoryValue(0x7ffffff - i, 0xde)
+#     ctx.setConcreteMemoryValue(0x7ffffff + i, 0xbe)
+
+# SymbolicVariable symbo lizeMemory(MemoryAccess mem, string symVarAlias)
+a = ctx.symbolizeMemory(MemoryAccess(0x7ffffff - 4, 4), "-4")
+b = ctx.symbolizeMemory(MemoryAccess(0x7ffffff - 8, 4), "-8")
+c = ctx.symbolizeMemory(MemoryAccess(0x7ffffff - 12, 4), "-12")
+d = ctx.symbolizeMemory(MemoryAccess(0x7ffffff - 16, 4), "-16")
+print(ctx.isMemorySymbolized(0x7ffffff - 4))
+
+# ctx.setAstRepresentationMode(AST_REPRESENTATION.PYTHON)
 
 for (addr, opcode) in code1:
     inst = Instruction()
@@ -17,6 +34,11 @@ for (addr, opcode) in code1:
     ctx.processing(inst)
     for expr in inst.getSymbolicExpressions():
             print(expr)
+    print("###", ctx.getSymbolicMemory(0x7ffffff - 4))
+
+for i in range(6):
+    print(hex(ctx.getConcreteMemoryValue(0x7ffffff - i)))
+    print(hex(ctx.getConcreteMemoryValue(0x7ffffff + i)))
 
 print()
 print("###########################################")
