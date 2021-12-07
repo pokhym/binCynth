468d55f3d9f701e1dcb899d86f4b2336809be63f
pokhym <ng121@purdue.edu>
2021-12-07 03:55:46 -0500

    commented out pointer stuff finalize full int first

--- synth_engine/components.cpp
+++ synth_engine/components.cpp
@@ -11,14 +11,14 @@ int int_sub(int a, int b){
 int int_mul(int a, int b){
     return a * b;
 }
-int intp_mod(int * p, int idx, int val){
-    if(idx < STATIC_MEMORY_MAX_SIZE && idx >= 0){
-        p[idx] = val;
-        return 0;
-    }
-    else
-        return -1;
-}
+// int intp_mod(int * p, int idx, int val){
+//     if(idx < STATIC_MEMORY_MAX_SIZE && idx >= 0){
+//         p[idx] = val;
+//         return 0;
+//     }
+//     else
+//         return -1;
+// }
 int synthed_4(int in_1){
 	int out_0 = int_add(1, in_1);
 	return out_0;
